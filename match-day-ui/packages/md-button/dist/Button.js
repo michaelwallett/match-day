cabcc96f96489eb866a48c34d8ac1bfbc46d97ce
Michael Wallett <michael.wallett@gmail.com>
2017-07-14 13:38:00 +0100

    fix

--- match-day-ui/packages/md-button/dist/Button.js
+++ match-day-ui/packages/md-button/dist/Button.js
@@ -6,8 +6,6 @@ Object.defineProperty(exports, "__esModule", {
 
 var _templateObject = _taggedTemplateLiteral(['\n  font-size: 1.5em;\n  margin: 1em;\n  padding: 0.25em 1em;\n  border: 2px solid palevioletred;\n  border-radius: 3px;\n  background: ', ';\n  color: ', ';\n'], ['\n  font-size: 1.5em;\n  margin: 1em;\n  padding: 0.25em 1em;\n  border: 2px solid palevioletred;\n  border-radius: 3px;\n  background: ', ';\n  color: ', ';\n']);
 
-exports.default = Button;
-
 var _styledComponents = require('styled-components');
 
 var _styledComponents2 = _interopRequireDefault(_styledComponents);
@@ -18,23 +16,12 @@ var _mdColours2 = _interopRequireDefault(_mdColours);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
-
 function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
-var StyledButton = _styledComponents2.default.button(_templateObject, function (props) {
+var Button = _styledComponents2.default.button(_templateObject, function (props) {
   return props.primary ? _mdColours2.default.primary : _mdColours2.default.secondary;
 }, function (props) {
   return props.primary ? 'white' : _mdColours2.default.primary;
 });
 
-function Button(_ref) {
-  var children = _ref.children,
-      props = _objectWithoutProperties(_ref, ['children']);
-
-  return React.createElement(
-    StyledButton,
-    props,
-    children
-  );
-}
+exports.default = Button;
