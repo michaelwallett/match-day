f9b4e2be41cd95fcbd69239ca1262e16d1d1cf0c
Michael Wallett <michael.wallett@gmail.com>
2017-07-14 11:22:38 +0100

    do better

--- match-day-ui/packages/md-button/dist/index.js
+++ match-day-ui/packages/md-button/dist/index.js
@@ -10,14 +10,18 @@ var _styledComponents = require('styled-components');
 
 var _styledComponents2 = _interopRequireDefault(_styledComponents);
 
+var _mdColours = require('md-colours');
+
+var _mdColours2 = _interopRequireDefault(_mdColours);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
 var Button = _styledComponents2.default.button(_templateObject, function (props) {
-  return props.primary ? 'palevioletred' : 'white';
+  return props.primary ? _mdColours2.default.primary : _mdColours2.default.secondary;
 }, function (props) {
-  return props.primary ? 'white' : 'palevioletred';
+  return props.primary ? 'white' : _mdColours2.default.primary;
 });
 
 exports.default = Button;
--- match-day-ui/packages/md-colours/src/index.js
+++ match-day-ui/packages/md-colours/src/index.js
@@ -1,4 +1,4 @@
-exports.default = {
+export default {
   primary: 'palevioletred',
   secondary: 'paleturquoise'
 };
