7b88d905d725d12b56e58a4aeafa398c00c455bf
Michael Wallett <michael.wallett@gmail.com>
2017-07-14 12:25:37 +0100

    fix

--- match-day-ui/packages/md-button/dist/index.js
+++ match-day-ui/packages/md-button/dist/index.js
@@ -4,7 +4,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _Button = require('./Button.jsx');
+var _Button = require('./Button');
 
 Object.defineProperty(exports, 'default', {
   enumerable: true,
